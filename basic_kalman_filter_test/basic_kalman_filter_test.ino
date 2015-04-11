ce764758fc0c97d350e80a1e01ed39fda6dcb372
Howard Yang <howard.yang96@gmail.com>
2015-04-10 23:34:30 -0500

    modified kalman test, added kalman function to main sketch

--- basic_kalman_filter_test/basic_kalman_filter_test.ino
+++ basic_kalman_filter_test/basic_kalman_filter_test.ino
@@ -1,34 +1,3 @@
-// I2C device class (I2Cdev) demonstration Arduino sketch for MPU9150
-// 1/4/2013 original by Jeff Rowberg <jeff@rowberg.net> at https://github.com/jrowberg/i2cdevlib
-//          modified by Aaron Weiss <aaron@sparkfun.com>
-//
-// Changelog:
-//     2011-10-07 - initial release
-//     2013-1-4 - added raw magnetometer output
-
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
-
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #include "Wire.h"
@@ -47,6 +16,8 @@ MPU6050 accelgyro;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 int16_t mx, my, mz;
+
+float Gx,Gy,Gz;
 
 #define LED_PIN 13
 bool blinkState = false;
@@ -115,25 +86,23 @@ void loop() {
     // read raw accel/gyro measurements from device
     accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
     
-    //update the estimate for the ax
-    kalman_update(&state, ax);
+    Gx = (gx * 250.0f / 32768.0f);
+    Gy = (gy * 250.0f / 32768.0f);
+    Gz = (gz * 250.0f / 32768.0f);
+ 
+    kalman_update(&state,Gx);
+    kalman_update(&state,Gy);
+    kalman_update(&state,Gz);
 
     // these methods (and a few others) are also available
     //accelgyro.getAcceleration(&ax, &ay, &az);
     //accelgyro.getRotation(&gx, &gy, &gz);
 
     // display tab-separated accel/gyro x/y/z values
-    Serial.print("original a, filtered a:\t");
-    Serial.print(ax); Serial.print("\t");
-    Serial.print(state.x);
-    //Serial.print(ay); Serial.print("\t");
-    //Serial.print(az); Serial.print("\t");
-    //Serial.print(gx); Serial.print("\t");
-    //Serial.print(gy); Serial.print("\t");
-    //Serial.print(gz); Serial.print("\t");
-    //Serial.print(mx); Serial.print("\t");
-    //Serial.print(my); Serial.print("\t");
-    //Serial.println(mz);
+    Serial.print("Gx: ");Serial.print(Gx);Serial.print("\t");
+    Serial.print("Gy: ");Serial.print(Gy);Serial.print("\t");
+    Serial.print("Gz: ");Serial.print(Gz);Serial.print("\t");
+    Serial.println();
 
     // blink LED to indicate activity
     blinkState = !blinkState;
