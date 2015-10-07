da6af1c10630d829e52c9d9138c2eaf68790d593
kennethdhau <kenneth.d.hau@gmail.com>
2015-10-06 19:48:13 -0500

    Refer to general_log.txt

--- PID_Test_wAccel/parse_output.py
+++ PID_Test_wAccel/parse_output.py
@@ -31,11 +31,62 @@ def parse_data_to_list(fname):
     sp2 = re.compile(r"Speed2:\t(\d*)\t")   ## no decimals appear so \d suffices
     speed_4 = []
     sp4 = re.compile(r"Speed4:\t(\d*)")     ## due to .strip() call, no final newline appears
-    for text_block in timestep_blocks:
-        accel_x.append(ax.search(text_block).group(1))
-        accel_y.append(ay.search(text_block).group(1))
-        accel_z.append(az.search(text_block).group(1))
-        x_output.append(xo.search(text_block).group(1))
-        speed_2.append(sp2.search(text_block).group(1))
-        speed_4.append(sp4.search(text_block).group(1))
+    try:
+        for text_block in timestep_blocks:
+            accel_x.append(ax.search(text_block).group(1))
+            accel_y.append(ay.search(text_block).group(1))
+            accel_z.append(az.search(text_block).group(1))
+            x_output.append(xo.search(text_block).group(1))
+            speed_2.append(sp2.search(text_block).group(1))
+            speed_4.append(sp4.search(text_block).group(1))
+    except AttributeError:
+        print("Detected EOF or other anomoly")
     return [accel_x, accel_y, accel_z, x_output, speed_2, speed_4]
+
+def write_data_to_file(fname, oname):
+    data_matrix = parse_data_to_list(fname)   ## in format of Ax, Ay, Az, Xo, S2, S4
+    data_header = "Acceleration (x)","Acceleration (y)","Acceleration (z)","PID Output Signal (x)","Speed Controller 2","Speed Controller 4"
+    with open(oname, 'w') as ofile:
+        for data_column_number in range(len(data_matrix)):
+            print(data_header[data_column_number],file=ofile)       ## print header name
+
+            for data_element in data_matrix[data_column_number]:    ## iterate over individual data elements in particular column
+                print(data_element, file=ofile)                     ## no newline required as print automatically writes newline
+
+            print("----------------",file=ofile)
+            print("----------------",file=ofile)                    ## print BOM-like seperator to seperate segments
+            print("----------------",file=ofile)
+
+def ascii_coordinate_from_duple(duple,letters):
+    return '%s%s'%(letters[duple[1]],duple[0])
+
+def write_data_to_ods_template(fname, oname='-1', tname="./spreadsheet_data_template.ods"):
+    if oname == '-1':
+        from time import strftime
+        oname = './%s.ods'%(strftime('%Y%m%d-%H%M%S'))
+    import ezodf        ## requires ezodf (pip install ezodf)
+    from string import ascii_uppercase as letters   ## requires string to reformat duple to ascii
+    data_matrix = parse_data_to_list(fname)   ## in format of Ax, Ay, Az, Xo, S2, S4
+    data_header = [fname,"Acceleration (x)","Acceleration (y)","Acceleration (z)","PID Output Signal (x)","Speed Controller 2","Speed Controller 4"]
+    derived_header = ["PID Output Signal (x) (1000)","Speed Controller 2 (1/10000)","Speed Controller 4 (1/10000)"]
+    formula_list = ['of:=[.%s]*1000','of:=[.%s]/10000','of:=[.%s]/10000']
+    data_range = range(1,len(data_matrix[0])+1)     ## Assumes a rectangular matrix of form n x m (returning m value iterator)
+    header_range = range(len(data_header))
+    derived_header_range = range(len(data_header),len(data_header)+len(derived_header))
+    spreadsheet = ezodf.opendoc(tname)
+    target_sheet = spreadsheet.sheets['Sheet1']
+    for column_number in header_range:
+        target_sheet[(0,column_number)].set_value(data_header[column_number])
+    for column_number in derived_header_range:
+        target_sheet[(0,column_number)].set_value(derived_header[column_number-len(data_header)])
+    for row_number in data_range:
+        for column_number in header_range[:-1]:
+            try:
+                target_sheet[(row_number,column_number+1)].set_value(float(data_matrix[column_number][row_number-1]))
+            except IndexError:
+                target_sheet.append_rows()
+                target_sheet[(row_number,column_number+1)].set_value(float(data_matrix[column_number][row_number-1]))
+        target_sheet[(row_number,0)].set_value(row_number)
+        for temp_column_number,formula_string in enumerate(formula_list):
+            target_sheet[(row_number,column_number+2+temp_column_number)].formula=formula_string%(ascii_coordinate_from_duple((row_number+1,column_number+temp_column_number-1),letters))
+    spreadsheet.saveas(oname)
