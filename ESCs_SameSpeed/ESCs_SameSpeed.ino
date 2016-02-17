3359806e4ab97ba5307f8d50644734737703b84c
howardyang96 <howard.yang96@gmail.com>
2016-02-16 22:49:52 -0600

    DMP6 implementation test

--- ESCs_SameSpeed/ESCs_SameSpeed.ino
+++ ESCs_SameSpeed/ESCs_SameSpeed.ino
@@ -12,21 +12,33 @@ void setup() {
   secondESC.attach(12);
   thirdESC.attach(11);
   fourthESC.attach(10);
+  firstESC.writeMicroseconds(1000);
+  secondESC.writeMicroseconds(1000);
+  thirdESC.writeMicroseconds(1000);
+  fourthESC.writeMicroseconds(1000);
   Serial.begin(9600);    // start serial at 9600 baud
-  delay(3000);
+  Serial.println(F("\nSend any character to begin: "));
+  while (Serial.available() && Serial.read()); // empty buffer
+  while (!Serial.available());                 // wait for data
+  while (Serial.available() && Serial.read()); // empty buffer again
+  Serial.println("Ready to parse:");
 
 }
 
 void loop() {
 
 //First connect your ESC WITHOUT Arming. Then Open Serial and follo Instructions
- 
-  firstESC.writeMicroseconds(value);
+
+  //int value = 1200;
+  
+  //firstESC.writeMicroseconds(value);
   secondESC.writeMicroseconds(value);
-  thirdESC.writeMicroseconds(value);
+  //thirdESC.writeMicroseconds(value);
   fourthESC.writeMicroseconds(value);
   
-  if(Serial.available()) 
+  if(Serial.available()) {
     value = Serial.parseInt();    // Parse an Integer from Serial
-
+    Serial.println(value);
+  }
+  
 }
